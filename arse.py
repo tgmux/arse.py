90ba5b8808f9294d39ba265a8b08e9362bfe5c37
toakley <toakley@ops.gg>
2015-11-16 18:19:16 -0500

    initial whack at support for the following:   elb            - EC2 Elastic Loadbalancer List   keys           - EC2 SSH Keys   security       - EC2 Security Groups   sg-xxxxxxxx    - Verbose EC2 Security Group Display   volumes        - EBS Volumes
    partially implemented:
      elb-<name>     - Verbose EC2 ELB Display

--- arse.py
+++ arse.py
@@ -5,7 +5,127 @@ import collections
 from colorama import Fore, Back, Style, init
 import re
 import sys
+#
+# Request EC2 Elastic Loadbalancers from AWS API
+def getEc2Elbs(ec2, elbName):
+	try:
+		if elbName == '':
+			loadBalancers = ec2.describe_load_balancers()
+		else:
+			loadBalancers = ec2.describe_load_balancers(LoadBalancerNames=[elbName])
+	except Exception as e:
+		sys.exit("Elastic Load Balancer query failure: " + str(e[0]))
 
+	elbs = []
+	for lb in loadBalancers['LoadBalancerDescriptions']:
+		elb = arsedefs.Ec2Elb(lb['LoadBalancerName'])
+		elb.dnsName = lb['DNSName'],
+		elb.vpcId = lb['VPCId'],
+		elb.zones = lb['AvailabilityZones']
+
+		# Loop over instances to which ELB is attached
+		instances = []
+		for instance in lb['Instances']:
+			instances.append(instance['InstanceId'])
+		elb.instances = instances
+
+		elbListeners = []
+		# Iterate & display through each listener of the ELB in question
+		for listener in lb['ListenerDescriptions']:
+			elbListener = arsedefs.Ec2ElbListener()
+			elbListener.lbPort = listener['Listener']['LoadBalancerPort']
+			elbListener.lbProtocol = listener['Listener']['Protocol']
+			elbListener.instancePort = listener['Listener']['InstancePort']
+			elbListener.instanceProtocol = listener['Listener']['InstanceProtocol']
+			elbListeners.append(elbListener)
+		elb.listeners = elbListeners
+		elbs.append(elb)
+
+	#if elbName == '':
+	return elbs
+	# else:
+	# 	return elbs[0]
+#
+# Request EC2 SSH Key Pairs from AWS API
+def getEc2KeyPairs(ec2):
+	try:
+		pairs = ec2.describe_key_pairs()
+	except Exception as e:
+		sys.exit("Key pair query failure: " + str(e[0]))
+
+	keyPairs = []
+	for key in pairs['KeyPairs']:
+		keyPair = arsedefs.Ec2KeyPair()
+		keyPair.name = key['KeyName']
+		keyPair.fingerprint = key['KeyFingerprint']
+		keyPairs.append(keyPair)
+
+	return keyPairs
+#
+# Request EC2 security groups from AWS API
+def getEc2SecurityGroups(ec2, securityGroupId):
+	try:
+		if securityGroupId == "all":
+			securityGroups = ec2.describe_security_groups()
+		else:
+			securityGroups = ec2.describe_security_groups(GroupIds=[securityGroupId])
+	except Exception as e:
+		sys.exit("Security groups query failure: " + str(e[0]))
+
+	# Array of returned EC2 security group objects
+	groups = []
+	for group in securityGroups['SecurityGroups']:
+		securityGroup = arsedefs.Ec2SecurityGroup(group['GroupId'])
+		securityGroup.name = group['GroupName']
+		securityGroup.description = group['Description']
+
+		# There are two Security Group Types. One for ingress and another for egress. 
+		groupTypes = ['IpPermissionsEgress', 'IpPermissions']
+		groupPermissions = []
+		for groupType in groupTypes:
+			# Let's create an array of EC2 security group objects
+			for permission in group[groupType]:
+				groupPermission = arsedefs.Ec2SecurityGroupPermission()
+				if 'FromPort' in permission:
+					groupPermission.fromPort = permission['FromPort']
+				else:
+					groupPermission.fromPort = 'all'
+				
+				if 'ToPort' in permission:
+					groupPermission.toPort = permission['ToPort']
+				else:
+					groupPermission.fromPort = 'all'
+				
+				groupPermission.protocol = permission['IpProtocol']
+				if groupType == 'IpPermissionsEgress':
+					groupPermission.type = 'outgoing'
+				elif groupType == 'IpPermissions': 
+					groupPermission.type = 'incoming'
+
+				ranges = []
+				# Catches lists of IPs
+				if len(permission['IpRanges']) > 1:
+					for cidr in permission['IpRanges']:
+						ranges.append(cidr['CidrIp'])
+				# This is pretty much to catch 0.0.0.0/0
+				elif len(permission['IpRanges']) == 1:
+					ranges.append(permission['IpRanges'][0]['CidrIp'])
+				# When objects beside cidr ranges appear, we don't handle those yet
+				else:
+					ranges = "n/a"
+
+				# Append array of IP ranges to group permission object
+				groupPermission.ranges = ranges
+				groupPermissions.append(groupPermission)
+			# Append array of permissions objects to the security group object
+			securityGroup.permissions = groupPermissions
+		# Append security group object to array of security groups
+		groups.append(securityGroup)
+
+	if securityGroupId == "all":
+		return groups
+	else:
+		return groups[0]
 # Get a list of volumes or a single volumes and return an Ec2Volume object
 #  or array of Ec2Volume objects
 def getEc2Volume(ec2, volumeId):
@@ -17,10 +137,7 @@ def getEc2Volume(ec2, volumeId):
 	except Exception as e:
 		sys.exit("Volumes query failure: " + str(e[0]))
 
-	# If we request all volumes, we return an array
-	if volumeId == "all":
-		returnedVolumes = []
-
+	returnedVolumes = []
 	# Always an array, even of 1. Iterate through any volumes returned.
 	for volume in volumes['Volumes']:
 		returnedVolume = arsedefs.Ec2Volume(volume['VolumeId'])
@@ -45,30 +162,26 @@ def getEc2Volume(ec2, volumeId):
 		returnedVolume.state = volume['State']
 		returnedVolume.tagName = tagName
 
-		if volumeId == "all":
-			returnedVolumes.append(returnedVolume)
+		returnedVolumes.append(returnedVolume)
 
 	# If we asked for all, return an array of Ec2Volumes or just one
 	if volumeId == "all":
 		return returnedVolumes
 	else:
-		return returnedVolume
+		return returnedVolumes[0]
 #
 #
-def getEc2KeyPairs(ec2):
+def displayEc2Elbs(ec2, lbName):
 	try:
-		pairs = ec2.describe_key_pairs()
+		elbs = getEc2Elbs(ec2, lbName)
 	except Exception as e:
-		sys.exit("Key pair query failure: " + str(e[0]))
+		sys.exit("getEc2Elbs query failure: " + str(e[0]))
 
-	keyPairs = []
-	for key in pairs['KeyPairs']:
-		keyPair = arsedefs.Ec2KeyPair()
-		keyPair.name = key['KeyName']
-		keyPair.fingerprint = key['KeyFingerprint']
-		keyPairs.append(keyPair)
+	print("{0:<13} {1:<16} {2:<29} {3}".format('VPC ID:', 'ELB Name:', 'Zones:', 'Public DNS Name:'))
+	print("===================================================================================================")
 
-	return keyPairs
+	for elb in elbs:
+		elb.printShort()		
 #
 #
 def displayEc2KeyPairs(ec2):
@@ -81,19 +194,36 @@ def displayEc2KeyPairs(ec2):
 	print "========================================================================"
 
 	for keyPair in keyPairs:
-		keyPair.display()
+		keyPair.printLong()
+#
+#
+def displayEc2SecurityGroups(ec2, securityGroupId):
+	#try:
+	groups = getEc2SecurityGroups(ec2, securityGroupId)
+	# except Exception as e:
+	# 	sys.exit("Get Security Groups query failure: " + str(e[0]))
+
+	if isinstance(groups, collections.Sequence):
+		print " ID:          Name:                    Description"
+		print "================================================================="
+
+		for group in groups:
+			group.printShort()
+	else:
+		groups.printLong()
 #
 # 
-def displayEc2Volume(ec2, volumeId):
+def displayEc2Volumes(ec2, volumeId):
 	try:
 		volumes = getEc2Volume(ec2, volumeId)
 	except Exception as e:
 		sys.exit("getVolumes query failure: " + str(e[0]))
 
 	if isinstance(volumes, collections.Sequence):
-		print ("{0:<12}  {1:<24} {2:<4} {3:<9} {4:<8} {5:<10} {6}".format(
-			"ID:", "Attached:", "GB:", "Device:", "Status:", "Zone:", "Name:"))
+		print " ID:           Attached:                GB:  Device:    Status:   Zone:       Name:"
 		print "===================================================================================================="
+		#print ("-[{0}]----------[{1}]--------------[{2}]--[{3}]--[{4}]---[{5}]-----[{6}]----------------------".format(
+		#	"ID", "Attached", "GB", "Device", "Status", "Zone", "Name"))
 
 		for volume in volumes:
 			volume.printShort()
@@ -104,27 +234,41 @@ def main():
 	# Let's be sure we get a command line option
 	clOption = ''
 	if len(sys.argv) < 2:
-		sys.exit("no")
+		arsedefs.printHelp()
 	else:
 		clOption = sys.argv[1]
 
-	# Parse said command line option
-	if clOption == "" or re.search('(-)?(-)?h(elp)?', clOption):
-		arsedefs.printHelp()
-	else: 
-		# Initialize the client object
-		ec2 = boto3.client('ec2')
-
-		# We don't need no stinkin argparse
-		if clOption == "volumes":
-			displayEc2Volume(ec2, 'all')
-		elif re.search('^vol\-', clOption):
-			displayEc2Volume(ec2, clOption)
-		elif clOption == "keys":
-			displayEc2KeyPairs(ec2)
-		else:
-			print "Error: invalid option."
+		# Parse said command line option
+		if clOption == "" or re.search('^(-)?(-)?h(elp)?', clOption):
 			arsedefs.printHelp()
+		else: 
+			# Initialize the AWS client object
+			if re.search('^elb', clOption):
+				ec2 = boto3.client('elb')
+			else:
+				ec2 = boto3.client('ec2')
+
+			# We don't need no stinkin argparse
+			#
+			# Elastic Loadbalancers
+			if clOption == "elb":
+				displayEc2Elbs(ec2, '')
+			elif re.search('^elb\-', clOption):
+				displayEc2Elbs(ec2, clOption.lstrip('elb-'))
+			# 
+			elif clOption == "keys":
+				displayEc2KeyPairs(ec2)
+			elif clOption == "security":
+				displayEc2SecurityGroups(ec2, 'all')
+			elif re.search('^sg\-', clOption):
+				displayEc2SecurityGroups(ec2, clOption)				
+			elif clOption == "volumes":
+				displayEc2Volumes(ec2, 'all')
+			elif re.search('^vol\-', clOption):
+				displayEc2Volumes(ec2, clOption)
+			else:
+				print "Error: invalid option."
+				arsedefs.printHelp()
 
 	print ""
 #
--- arsedefs.py
+++ arsedefs.py
@@ -1,3 +1,5 @@
+from colorama import Fore, Back, Style, init
+
 class Ec2KeyPair:
 	'Common base class for EC2 key pairs'
 
@@ -5,11 +7,10 @@ class Ec2KeyPair:
 		self.name = ''
 		self.fingerprint = ''
 
-	def display(self):
+	def printLong(self):
 		print("{name:<12s} {fingerprint}".format(
 			name=self.name,
 			fingerprint=self.fingerprint))
-
 
 class Ec2Instance:
 	'Common base class for EC2 Instances'
@@ -17,6 +18,87 @@ class Ec2Instance:
 	def __init__(self, instanceId):
 		self.instanceId = instanceId
 
+class Ec2Elb:
+	'Common base class for EC2 Elastic Loadbalancers'
+
+	def __init__(self, elbName):
+		self.elbName = elbName
+		self.dnsName = ''
+		self.instances = []
+		self.listeners = []
+		self.vpcId = ''
+		self.zones = []
+
+	def printShort(self):
+		print ("{vpcid:<13} {lbname:<24} {zones:<29} {dnsname}".format(
+			lbname=Style.BRIGHT + self.elbName + Style.RESET_ALL,
+			dnsname=self.dnsName[0],
+			vpcid=self.vpcId[0],
+			zones=str(self.zones)))
+
+	def printLong(self):
+		print ("{vpcid:<13} {lbname:<24} {zones:<28} {dnsname}".format(
+			lbname=Style.BRIGHT + self.elbName + Style.RESET_ALL,
+			dnsname=self.dnsName[0],
+			vpcid=self.vpcId[0],
+			zones=str(self.zones)))
+
+		print "- " + str(self.instances)
+
+		for listener in self.listeners:
+			print("    {lbprotocol:<14} {lbport:<15} -> {iprotocol:<14} {iport:<15}".format(
+				lbport=listener.lbPort,
+				lbprotocol=listener.lbProtocol,
+				iport=listener.instancePort,
+				iprotocol=listener.instanceProtocol))
+
+
+
+class Ec2ElbListener:
+	def __init__(self):
+		self.instancePort = ''
+		self.instanceProtocol = ''
+		self.lbPort = ''
+		self.lbProtocol = ''
+
+class Ec2SecurityGroup:
+	'Common base class for EC2 Security Groups'
+	
+	def __init__(self, securityGroupId):
+		self.securityGroupId = securityGroupId
+		self.name = ''
+		self.description = ''
+		self.permissions = []
+
+	def printShort(self):
+		print(" {id:<12s} {name:<24s} {description}").format(
+			name=self.name,
+			id=self.securityGroupId,
+			description=self.description)
+	
+	def printLong(self):
+		print(" {id:<12s} {name:<24s} {description}\n").format(
+			name=self.name,
+			id=self.securityGroupId,
+			description=self.description)
+		
+		for permission in self.permissions:
+			print ("  {type:<9} {protocol:<14}  {fromPort:<6}  {toPort:<6} {ranges}".format(
+				type=permission.type,
+				protocol=Fore.CYAN + permission.protocol + Fore.RESET,
+				fromPort=permission.fromPort,
+				toPort=permission.toPort,
+				ranges=permission.ranges))
+
+class Ec2SecurityGroupPermission:
+	'EC2 Security Group Permission Data Structure'
+
+	def __init__(self):
+		self.type = ''
+		self.fromPort = ''
+		self.toPort = ''
+		self.protocol = ''
+		self.ranges = []
 
 class Ec2Volume:
 	'Class to describe EC2 EBS Volumes'
@@ -39,7 +121,7 @@ class Ec2Volume:
 	def printShort(self):
 		self.combinedInstanceName = (self.attached['attachInstanceId'] + 
 			" (" + str(self.attached['attachHostname']) + ")")
-		print ("{volumeId:<12}  {instance:<24} {size:<4} {device:<9} {state:<8} {zone} {tagname}".format(
+		print (" {volumeId:<12}  {instance:<24} {size:<4} {device:<10} {state:<9} {zone}  {tagname}".format(
 				zone=self.availabilityZone,
 				volumeId=self.volumeId,
 				instance=self.combinedInstanceName,
@@ -57,9 +139,18 @@ class Ec2Volume:
 				tagname=self.tagName)
 
 def printHelp():
-	print "\narse :: Amazon Resource Explorer"
-	print "---------------------------------"
-	print "  keys        - List EC2 SSH Keys"
-	print "  volumes     - List EBS volumes"
-	print "---------------------------------"
-	print "ex: arse [options] [command]"
+	print "\narse :: Amazon ReSource Explorer"
+	print "-------------------------------------------------------"
+	print "  elb            - EC2 Elastic Loadbalancer List"
+	print "  elb-<name>     - Verbose EC2 ELB Display"
+	print "  *images        - EC2 AMI List"
+	print "  *ami-xxxxxxxx  - Verbose EC2 AMI Display"
+	print "  *instances     - EC2 Instance List"
+	print "  *i-xxxxxxxx    - Verbose EC2 Instance Display"
+	print "  keys           - EC2 SSH Keys"
+	print "  security       - EC2 Security Groups"
+	print "  sg-xxxxxxxx    - Verbose EC2 Security Group Display"
+	print "  volumes        - EBS Volumes"
+	print "  *vol-xxxxxxxx  - Verbose EBS Volume Display"
+	print "-------------------------------------------------------"
+	print "ex: arse [command]"
